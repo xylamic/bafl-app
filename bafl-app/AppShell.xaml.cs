3b1d5fb0759b73f957853d3eee9395f268fdd874
Adam Jordan <adam.jordan@shell.com>
2022-10-30 10:16:06 -0500

    First commit

---
+++ bafl-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace bafl_app;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ bafl-app/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace bafl_app;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
