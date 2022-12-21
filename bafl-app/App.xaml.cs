045d5d90261fb99880b41a43b8b833a9ebc01b8b
Adam Jordan <adam.jordan@shell.com>
2022-12-20 23:41:53 -0600

    added board data read

--- bafl-app/App.xaml.cs
+++ bafl-app/App.xaml.cs
@@ -8,6 +8,9 @@ public partial class App : Application
 	private static string TEAM_URL = "https://xylamic.github.io/bafl-app/team-list.json";
 	private static string TEAM_FILE_NAME = "team-list.json";
 
+    private static string BOARD_URL = "https://xylamic.github.io/bafl-app/board-list.json";
+    private static string BOARD_FILE_NAME = "board-list.json";
+
     public App()
 	{
 		InitializeComponent();
@@ -20,6 +23,8 @@ public partial class App : Application
 
     public static Dictionary<int, BAFLClub> ClubList { get; private set; }
 
+    public static List<BaflBoardMember> BoardMemberList { get; private set; }
+
     public static async Task UpdateConfiguration()
     {
         if (IsInitiatied)
@@ -27,6 +32,7 @@ public partial class App : Application
 
         string cacheDir = FileSystem.Current.CacheDirectory;
 
+        #region Team List
         // if the team data does not already exist, then load from assets
         if (!File.Exists(cacheDir + "/" + TEAM_FILE_NAME))
         {
@@ -61,6 +67,44 @@ public partial class App : Application
         ClubList =
             JsonSerializer.Deserialize<Dictionary<int, BAFLClub>>(content);
         Console.WriteLine("Loaded team data.");
+        #endregion
+
+        #region Board List
+        // if the board data does not already exist, then load from assets
+        if (!File.Exists(cacheDir + "/" + BOARD_FILE_NAME))
+        {
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("Board.json");
+            using StreamReader reader = new StreamReader(fileStream);
+            string rawContent = await reader.ReadToEndAsync();
+
+            await File.WriteAllTextAsync(cacheDir + "/" + BOARD_FILE_NAME, rawContent);
+
+            Console.WriteLine("Loaded raw asset board data.");
+        }
+
+        // read the most recent content, in case of web error
+        content = await File.ReadAllTextAsync(cacheDir + "/" + BOARD_FILE_NAME);
+
+        // try to read the latest from the website
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                content = await client.GetStringAsync(BOARD_URL);
+                await File.WriteAllTextAsync(cacheDir + "/" + BOARD_FILE_NAME, content);
+                Console.WriteLine("Successfully read latest board data.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(String.Format("Error reading board data: {0}", ex.ToString()));
+        }
+
+        // deserialize the teams
+        BoardMemberList =
+            JsonSerializer.Deserialize<List<BaflBoardMember>>(content);
+        Console.WriteLine("Loaded board data.");
+        #endregion
 
         IsInitiatied = true;
     }
