e46286e5420c5bcc64e92eb7c016c273a41c1390
Adam Jordan <adam.jordan@shell.com>
2022-12-20 23:08:22 -0600

    add board member

---
+++ bafl-library/BaflBoardMember.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+
+namespace bafl.library
+{
+    public class BaflBoardMember
+    {
+        private string _role;
+        private string _name;
+        private string _email;
+
+        public BaflBoardMember() { }
+
+        public BaflBoardMember(string role, string name, string email)
+        {
+            _role = role;
+            _name = name;
+            _email = email;
+        }
+
+        public string Role
+        {
+            get => _role;
+            set => _role = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public bool EmailPresent
+        {
+            get => !String.IsNullOrEmpty(Email);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{}: {}", Role, Name);
+        }
+    }
+}
+
