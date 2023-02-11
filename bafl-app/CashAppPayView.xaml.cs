4a5b9ecb7445da41e6b70118e680cd66a46cde2e
Adam Jordan <xylamic@xylasoft.com>
2023-02-10 22:17:25 -0600

    fixed the pay pages

--- bafl-app/CashAppPayView.xaml.cs
+++ bafl-app/CashAppPayView.xaml.cs
@@ -6,6 +6,11 @@ public partial class CashAppPayView : ContentPage
 	{
         InitializeComponent();
     }
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://cash.app/$bayareabucs?qr=1");
+    }
 }
 
 
--- bafl-app/ZellePayView.xaml.cs
+++ bafl-app/ZellePayView.xaml.cs
@@ -6,6 +6,11 @@ public partial class ZellePayView : ContentPage
 	{
         InitializeComponent();
     }
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+        await Browser.Default.OpenAsync("https://enroll.zellepay.com/qr-codes?data=eyJuYW1lIjoiQURBTSIsImFjdGlvbiI6InBheW1lbnQiLCJ0b2tlbiI6IjI4MTczMTE5MDQifQ==");
+    }
 }
 
 
