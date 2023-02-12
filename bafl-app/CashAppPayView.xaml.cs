c5602f5dce1e75f271a1f0b84bce4f65d72b5f24
Adam Jordan <xylamic@xylasoft.com>
2023-02-11 20:21:51 -0600

    Updated the pay pages

--- bafl-app/CashAppPayView.xaml.cs
+++ bafl-app/CashAppPayView.xaml.cs
@@ -9,7 +9,7 @@ public partial class CashAppPayView : ContentPage
 
     private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
     {
-        await Browser.Default.OpenAsync("https://cash.app/$bayareabucs?qr=1");
+        await Browser.Default.OpenAsync("https://cash.app/$payBAFL?qr=1");
     }
 }
 
--- bafl-app/ZellePayView.xaml.cs
+++ bafl-app/ZellePayView.xaml.cs
@@ -9,7 +9,7 @@ public partial class ZellePayView : ContentPage
 
     private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
     {
-        await Browser.Default.OpenAsync("https://enroll.zellepay.com/qr-codes?data=eyJuYW1lIjoiQURBTSIsImFjdGlvbiI6InBheW1lbnQiLCJ0b2tlbiI6IjI4MTczMTE5MDQifQ==");
+        await Browser.Default.OpenAsync("https://enroll.zellepay.com/qr-codes?data=ewogICJuYW1lIiA6ICJCQVkgQVJFQSBGT09UQkFMTCBMRUFHVUUgSU5DIiwKICAidG9rZW4iIDogImJhZmx0cmVhc3VyZXIyMUBnbWFpbC5jb20iLAogICJhY3Rpb24iIDogInBheW1lbnQiCn0=");
     }
 }
 
