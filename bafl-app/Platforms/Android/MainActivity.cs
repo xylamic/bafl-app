0ff8ff4b99f3ac20eb21499608c3e8133644e279
Adam Jordan <adam.jordan@shell.com>
2023-02-13 05:29:07 -0600

    Fixed to portrait, fixed spacing on mainpage

--- bafl-app/Platforms/Android/MainActivity.cs
+++ bafl-app/Platforms/Android/MainActivity.cs
@@ -4,7 +4,10 @@ using Android.OS;
 
 namespace bafl_app;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+    ScreenOrientation = ScreenOrientation.Portrait)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
