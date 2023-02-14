1c635969b6a68f24be11be2d8d0e72b836ca99b7
Adam Jordan <adam.jordan@shell.com>
2023-02-14 09:12:21 -0600

    simply initialization and added opacity transitions for old dates

--- bafl-app/App.xaml.cs
+++ bafl-app/App.xaml.cs
@@ -13,6 +13,11 @@ public partial class App : Application
     private static string BOARD_FILE_NAME = "board-list.json";
     private static string SCHEDULE_FILE_NAME = "schedule.json";
 
+    // cache file paths
+    private static string _team_cache_filename;
+    private static string _board_cache_filename;
+    private static string _schedule_cache_filename;
+
     /// <summary>
     /// Has the App been initiated.
     /// </summary>
@@ -30,7 +35,13 @@ public partial class App : Application
         BoardMemberList = new List<BaflBoardMember>();
         ScheduleList = new List<BaflScheduleItem>();
 
-		MainPage = new AppShell();
+        // get the cache file locations
+        string cacheDir = FileSystem.Current.CacheDirectory;
+        _team_cache_filename = cacheDir + "/" + TEAM_FILE_NAME;
+        _board_cache_filename = cacheDir + "/" + BOARD_FILE_NAME;
+        _schedule_cache_filename = cacheDir + "/" + SCHEDULE_FILE_NAME;
+
+        MainPage = new AppShell();
     }
 
     /// <summary>
@@ -52,7 +63,7 @@ public partial class App : Application
     /// Update the application configuration.
     /// </summary>
     /// <returns>The async task.</returns>
-    public static async Task UpdateConfiguration()
+    public static async Task LoadConfiguration()
     {
         if (_isInitiatied)
             return;
@@ -60,12 +71,21 @@ public partial class App : Application
         // verify cache files are ready
         await PreloadCacheCheck();
 
-        // get the cache file locations
-        string cacheDir = FileSystem.Current.CacheDirectory;
-        string team_cache_filename = cacheDir + "/" + TEAM_FILE_NAME;
-        string board_cache_filename = cacheDir + "/" + BOARD_FILE_NAME;
-        string schedule_cache_filename = cacheDir + "/" + SCHEDULE_FILE_NAME;
+        // get the latest files from the internet
+        await PullRemoteConfiguration();
 
+        // load the files
+        await LoadCacheFiles();
+
+        _isInitiatied = true;
+    }
+
+    /// <summary>
+    /// Update the configuration from the remote URLs.
+    /// </summary>
+    /// <returns>The async task.</returns>
+    public static async Task PullRemoteConfiguration()
+    {
         try
         {
             // try to read the latest configuration information.
@@ -75,9 +95,9 @@ public partial class App : Application
                 string boardContent = await client.GetStringAsync(BaflUtilities.BOARD_URL);
                 string scheduleContent = await client.GetStringAsync(BaflUtilities.SCHEDULE_URL);
 
-                await File.WriteAllTextAsync(team_cache_filename, teamContent);
-                await File.WriteAllTextAsync(board_cache_filename, boardContent);
-                await File.WriteAllTextAsync(schedule_cache_filename, scheduleContent);
+                await File.WriteAllTextAsync(_team_cache_filename, teamContent);
+                await File.WriteAllTextAsync(_board_cache_filename, boardContent);
+                await File.WriteAllTextAsync(_schedule_cache_filename, scheduleContent);
 
                 Console.WriteLine("Successfully read latest data from the APIs.");
             }
@@ -86,23 +106,26 @@ public partial class App : Application
         {
             Console.WriteLine(String.Format("Error reading API data: {0}", ex.ToString()));
         }
+    }
 
+    private static async Task LoadCacheFiles()
+    {
         try
         {
             // load team data
-            string content = await File.ReadAllTextAsync(team_cache_filename);
+            string content = await File.ReadAllTextAsync(_team_cache_filename);
             ClubList =
                 JsonSerializer.Deserialize<Dictionary<int, BaflClub>>(content);
             Console.WriteLine("Loaded team data.");
 
             // load board data
-            content = await File.ReadAllTextAsync(board_cache_filename);
+            content = await File.ReadAllTextAsync(_board_cache_filename);
             BoardMemberList =
                 JsonSerializer.Deserialize<List<BaflBoardMember>>(content);
             Console.WriteLine("Loaded board data.");
 
             // load schedule data
-            content = await File.ReadAllTextAsync(schedule_cache_filename);
+            content = await File.ReadAllTextAsync(_schedule_cache_filename);
             ScheduleList =
                 JsonSerializer.Deserialize<List<BaflScheduleItem>>(content);
             Console.WriteLine("Loaded schedule data.");
@@ -111,8 +134,6 @@ public partial class App : Application
         {
             Console.WriteLine(String.Format("Failed to load data: {0}", ex.ToString()));
         }
-
-        _isInitiatied = true;
     }
 
     /// <summary>
@@ -122,45 +143,38 @@ public partial class App : Application
     /// <returns>Async task.</returns>
     private static async Task PreloadCacheCheck()
     {
-        string cacheDir = FileSystem.Current.CacheDirectory;
-
-        // get the cache file locations
-        string team_cache_filename = cacheDir + "/" + TEAM_FILE_NAME;
-        string board_cache_filename = cacheDir + "/" + BOARD_FILE_NAME;
-        string schedule_cache_filename = cacheDir + "/" + SCHEDULE_FILE_NAME;
-
         // verify the team cache file exists
-        if (!File.Exists(team_cache_filename))
+        if (!File.Exists(_team_cache_filename))
         {
             using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("Teams.json");
             using StreamReader reader = new StreamReader(fileStream);
             string rawContent = await reader.ReadToEndAsync();
 
-            await File.WriteAllTextAsync(team_cache_filename, rawContent);
+            await File.WriteAllTextAsync(_team_cache_filename, rawContent);
 
             Console.WriteLine("Loaded raw asset team data.");
         }
 
         // verify the board cache file exists
-        if (!File.Exists(board_cache_filename))
+        if (!File.Exists(_board_cache_filename))
         {
             using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("Board.json");
             using StreamReader reader = new StreamReader(fileStream);
             string rawContent = await reader.ReadToEndAsync();
 
-            await File.WriteAllTextAsync(board_cache_filename, rawContent);
+            await File.WriteAllTextAsync(_board_cache_filename, rawContent);
 
             Console.WriteLine("Loaded raw asset board data.");
         }
 
         // verify the schedule cache file exists
-        if (!File.Exists(schedule_cache_filename))
+        if (!File.Exists(_schedule_cache_filename))
         {
             using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("Schedule.json");
             using StreamReader reader = new StreamReader(fileStream);
             string rawContent = await reader.ReadToEndAsync();
 
-            await File.WriteAllTextAsync(schedule_cache_filename, rawContent);
+            await File.WriteAllTextAsync(_schedule_cache_filename, rawContent);
 
             Console.WriteLine("Loaded raw asset schedule data.");
         }
